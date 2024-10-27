19a60928fa09881eb30a838bf43b697e54956cbb
Praseesh <150511158+praseesh@users.noreply.github.com>
2024-10-27 13:42:59 +0530

    Update settings.py

--- weather/settings.py
+++ weather/settings.py
@@ -89,13 +89,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.1/howto/static-files/
-
 STATIC_URL = 'static/'
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
